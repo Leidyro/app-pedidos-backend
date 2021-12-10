f36cc90ef18136e7909bfc8d2a93b6af45f98c4f
Leidy Rodriguez <leidytkm96@gmail.com>
2021-12-09 23:04:03 -0500

    prueba-cambios

--- src/models/pedido.model.ts
+++ src/models/pedido.model.ts
@@ -1,4 +1,4 @@
-import {Entity, model, property, belongsTo, hasOne} from '@loopback/repository';
+import {belongsTo, Entity, hasOne, model, property} from '@loopback/repository';
 import {Persona} from './persona.model';
 import {Producto} from './producto.model';
 
@@ -10,6 +10,8 @@ export class Pedido extends Entity {
     generated: true,
   })
   id?: string;
+
+  //prueba de cambios
 
   @property({
     type: 'string',
